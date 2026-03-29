27fe73e80d14b7b08a82f347959be743172e5064
Yash <acharkisabzi@gmail.com>
2026-03-29 19:34:35 +0530

    added username parameter in UserModel

--- app/src/main/java/com/example/buyerappdemo/models/UserModel.kt
+++ app/src/main/java/com/example/buyerappdemo/models/UserModel.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class UserModel(
     val id: String? = "",
+    val username: String = "",
     val name: String = "",
     val area: String = "",
     val email: String = "",
